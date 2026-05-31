c83e6f345634469336d4044197a571491052fc94
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2026-05-31 23:36:20 +0700

    Revert back to WDK 26100

--- scripts/build.bat
+++ scripts/build.bat
@@ -19,7 +19,7 @@ call :check cargo build -p rat-client --profile %profile%
 
 @REM We cannot set this in .cargo/config.toml because `cargo-wdk` spawns its own process and is not affected by the config file.
 @REM https://github.com/microsoft/windows-drivers-rs/blob/a90b267ccd9288d076ecbe96a7966f96f337bdc1/crates/wdk-build/src/utils.rs#L314-L328
-set Version_Number=10.0.19041.0
+set Version_Number=10.0.26100.0
 
 cd /d %root%\rat-driver
 call :check cargo wdk build -v --profile %profile%
