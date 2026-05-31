dafa88c282699013607b2861dc9b922423ec6475
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2026-05-31 21:20:51 +0700

    Check WDK version properly

--- scripts/build.bat
+++ scripts/build.bat
@@ -8,11 +8,14 @@ set profile=%~1
 if "%profile%"=="" set profile=dev
 echo Building with profile "%profile%"
 
+rustc --version
+cargo --version
+
 cd /d %root%
 cargo build -p rat-client --profile %profile%
 
 cd /d %root%\rat-driver
-cargo wdk build --profile %profile%
+cargo wdk build --verbose --profile %profile%
 
 cd /d %root%\rat-efi
 cargo build --profile %profile%
